2437dd0262d6bc56a056fbe8a90b1f8b19617115
Seven <sevenspell@gmail.com>
2020-04-26 12:53:34 +1000

    responsive css

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,11 +4,14 @@ import "./Footer.css"
 function Footer() {
     return (
         <div>
-            <p id="detailslink" className="fixed-bottom col-sm-12">
-                Click on the icons for my LinkedIn, Github and Resume.
-            </p>
-            <nav className="navbar fixed-bottom navbar-dark bg-dark" id="navbar">
-                <div id="footerfont">&copy; Rita Pang 2020</div>
+            {/* <p id="detailslink" className="fixed-bottom col-sm-12">
+                Credits: logo design @ <a href="https://logomakr.com/" target="blank">logomakr</a>
+            </p> */}
+            <nav className="navbar fixed-bottom col-sm-12" id="navbarfooter">
+            <div id="detailslink" className="fixed-bottom col-sm-12">
+                Credits: logo design @ <a href="https://logomakr.com/" target="blank">logomakr</a>
+            </div>
+                <div id="footerfont">&copy; This Is The End 2020</div>
             </nav>
         </div>
     )
