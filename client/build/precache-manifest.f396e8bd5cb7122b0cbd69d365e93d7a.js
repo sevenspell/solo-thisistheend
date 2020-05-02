c991c72d0799cd1feb1c98e30b16bb19b1f579fd
Seven <sevenspell@gmail.com>
2020-05-02 17:41:48 +1000

    removed /build from gitignore

---
+++ client/build/precache-manifest.f396e8bd5cb7122b0cbd69d365e93d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac04798abcb20112cc6c2083f2818460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4f33431f17a7534f39",
+    "url": "/static/css/main.6cd3c539.chunk.css"
+  },
+  {
+    "revision": "91cfe7f44fdcfa5870bf",
+    "url": "/static/js/2.25363c89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25363c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4f33431f17a7534f39",
+    "url": "/static/js/main.3b8fdcc4.chunk.js"
+  },
+  {
+    "revision": "453ce1f4ad68ab26bc8d",
+    "url": "/static/js/runtime-main.0800db55.js"
+  },
+  {
+    "revision": "03ab6465bca1c2c2dcf985b064ca72ab",
+    "url": "/static/media/sunlightphoto.03ab6465.JPG"
+  }
+]);
