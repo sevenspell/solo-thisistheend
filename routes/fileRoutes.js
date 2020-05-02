a6f5e1522914472db3cff90a0d80237fe0e8ea4c
Seven <sevenspell@gmail.com>
2020-05-02 13:09:03 +1000

    fileroutes

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -8,22 +8,22 @@ const cors = require('cors')
 router.use(cors());
 
 // file upload route for gameover page
-// router.post('/upload', function(req, res) {
-//     if (!req.files || Object.keys(req.files).length === 0) {
-//       return res.status(400).send('No files were uploaded.');
-//     }
+router.post('/upload', function(req, res) {
+    if (!req.files) {
+      return res.status(400).send('No files were uploaded.');
+    }
   
-//     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-//     let sampleFile = req.files.sampleFile;
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let uploadedFile = req.files.uploadedFile;
   
-//     // Use the mv() method to place the file somewhere on your server
-//     sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
-//       if (err)
-//         return res.status(500).send(err);
+    // Use the mv() method to place the file somewhere on your server
+    uploadedFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+      if (err)
+        return res.status(500).send(err);
   
-//       res.send('File uploaded!');
-//     });
-//   });
+      res.send('File uploaded!');
+    });
+  });
 
 
 module.exports = router
